da3ac3a3a5e7acf9f661358f80d58acff688077d
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2023-05-06 00:55:45 +0800

    feature:1. add google analysis

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,33 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+import React from 'react';
+import Script from 'next/script';
+
+const App = ({ Component, pageProps }) => {
+    return (
+        <>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-X7999CT0H3"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-X7999CT0H3');
+        `}
+            </Script>
+
+            <Component {...pageProps} />
+        </>
+    );
+};
+
+export default App;
+
