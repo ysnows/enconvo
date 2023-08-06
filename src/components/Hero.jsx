be051adb5701e26beab4f2810e2e327d217e5bf1
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2023-08-06 14:34:26 +0800

    feat: 1. optimize

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,10 +19,11 @@ function classNames(...classes) {
 const navigation = [{name: 'Features', href: '#features'}, {
     name: 'Pricing',
     href: '#pricing'
-}, {name: 'Telegram', href: 'https://t.me/+iHQntezKbVViMWE1'}, {
-    name: 'Developer',
-    href: '/developer'
-},]
+}, {name: 'Telegram', href: 'https://t.me/+iHQntezKbVViMWE1'},
+     {
+        name: 'Developer',
+        href: '/developer'
+    },]
 
 export function Hero() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
@@ -184,6 +185,7 @@ export function Hero() {
                                 Download For Mac
                             </a>
 
+
                             <div className="mt-4 text-sm font-normal text-gray-500 space-x-4">
                                 <span
                                     className=" pb-3  pl-1">v1.2.5</span>
@@ -209,13 +211,6 @@ export function Hero() {
                 className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
                 aria-hidden="true"
             >
-                {/*<div*/}
-                {/*    className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"*/}
-                {/*    style={{*/}
-                {/*        clipPath:*/}
-                {/*            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',*/}
-                {/*    }}*/}
-                {/*/>*/}
             </div>
         </div>
     </div>)
--- src/components/PrimaryFeatures.jsx
+++ src/components/PrimaryFeatures.jsx
@@ -5,6 +5,9 @@ import clsx from 'clsx'
 
 import {Container} from '@/components/Container'
 import comingSoon from '@/images/coming soon.png'
+import writing from '@/images/features/writing.gif'
+import plugin from '@/images/features/plugin.gif'
+import chain from '@/images/features/chain.gif'
 
 const features = [
     {
@@ -38,6 +41,7 @@ const features = [
 export function PrimaryFeatures() {
     let [tabOrientation, setTabOrientation] = useState('horizontal')
 
+
     useEffect(() => {
         let lgMediaQuery = window.matchMedia('(min-width: 1024px)')
 
@@ -59,119 +63,135 @@ export function PrimaryFeatures() {
             aria-label="Features for running your books"
             className="relative overflow-hidden bg-gray-900 pb-28 pt-20 sm:py-32"
         >
-            <Image
-                className="absolute left-1/2 top-1/2 max-w-none translate-x-[-44%] translate-y-[-42%]"
-                // src={backgroundImage}
-                alt=""
-                width={2245}
-                height={1636}
-                unoptimized
-            />
+
             <Container className="relative">
                 <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
                     <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl">
                         The future is just beginning.
                     </h2>
                     <p className="mt-6 text-lg tracking-tight text-blue-100">
-                        The future is full of possibilities, challenges and opportunities, and with innovation and collaboration, we can achieve remarkable breakthroughs.
+                        The future is full of possibilities, challenges and opportunities, and with
+                        innovation and collaboration, we can achieve remarkable breakthroughs.
                     </p>
                 </div>
-                <Tab.Group
-                    as="div"
-                    className="mt-16 grid grid-cols-1 items-center gap-y-2 pt-10 sm:gap-y-6 md:mt-20 lg:grid-cols-12 lg:pt-0"
-                    vertical={tabOrientation === 'vertical'}
-                >
-                    {({selectedIndex}) => (
-                        <>
-                            <div
-                                className="-mx-4 flex overflow-x-auto pb-4 sm:mx-0 sm:overflow-visible sm:pb-0 lg:col-span-5">
-                                <Tab.List
-                                    className="relative z-10 flex gap-x-4 whitespace-nowrap px-4 sm:mx-auto sm:px-0 lg:mx-0 lg:block lg:gap-x-0 lg:gap-y-1 lg:whitespace-normal">
-                                    {features.map((feature, featureIndex) => (
-                                        <div
-                                            key={feature.title}
-                                            className={clsx(
-                                                'group relative rounded-full px-4 py-1 lg:rounded-l-xl lg:rounded-r-none lg:p-6',
-                                                selectedIndex === featureIndex
-                                                    ? 'bg-white lg:bg-white/10 lg:ring-1 lg:ring-inset lg:ring-white/10'
-                                                    : 'hover:bg-white/10 lg:hover:bg-white/5'
-                                            )}
-                                        >
-                                            <h3>
-                                                <Tab
-                                                    className={clsx(
-                                                        'font-display text-lg [&:not(:focus-visible)]:focus:outline-none',
-                                                        selectedIndex === featureIndex
-                                                            ? 'text-blue-600 lg:text-white'
-                                                            : 'text-blue-100 hover:text-white lg:text-white'
-                                                    )}
-                                                >
-                                                    <span
-                                                        className="absolute inset-0 rounded-full lg:rounded-l-xl lg:rounded-r-none"/>
-                                                    {feature.title}
-                                                </Tab>
-                                            </h3>
-                                            <p
-                                                className={clsx(
-                                                    'mt-2 hidden text-sm lg:block',
-                                                    selectedIndex === featureIndex
-                                                        ? 'text-white'
-                                                        : 'text-blue-100 group-hover:text-white'
-                                                )}
-                                            >
-                                                {feature.description}
-                                            </p>
-                                        </div>
-                                    ))}
-                                </Tab.List>
-                            </div>
-                            <Tab.Panels className="lg:col-span-7">
-                                {features.map((feature) => (
-                                    <Tab.Panel key={feature.title} unmount={false}>
-                                        <div className="relative sm:px-6 lg:hidden">
-                                            <div
-                                                className="absolute -inset-x-4 bottom-[-4.25rem] top-[-6.5rem] bg-white/10 ring-1 ring-inset ring-white/10 sm:inset-x-0 sm:rounded-t-xl"/>
-                                            <p className="relative mx-auto max-w-2xl text-base text-white sm:text-center">
-                                                {feature.description}
-                                            </p>
-                                        </div>
-                                        <div
-                                            // sm:w-auto lg:mt-0 lg:w-[67.8125rem]
-                                            className="sm:w-auto lg:mt-0 mt-10 lg:w-[67.8125rem] overflow-hidden ">
 
-                                            {/* 判断如果feature.video存在则显示<video/>，否则显示<Image/>}
-                                            {/*实现上边的逻辑*/}
 
-                                            {feature.video
-                                                ? <video
-                                                    className="lg:w-10/12 sm:w-auto h-1/6 mt-10 lg:mt-0 rounded-xl"
-                                                    controls>
-                                                    <source src={feature.video} type="video/mp4"/>
-                                                    Your browser does not support the video tag.
-                                                </video>
-                                                :
-                                                <img
-                                                    className="lg:w-10/12 sm:w-auto sm:h-auto lg:h-1/6 mt-10 lg:mt-0 rounded-xl"
-                                                    src={feature.image}
-                                                    />
+                <div
+                    className="flex text-white font-mono mt-28 ">
 
-                                                // <Image
-                                                //     className="lg:w-10/12 sm:w-auto h-1/6 mt-10 lg:mt-0 rounded-xl"
-                                                //     src={"https://github.com/ysnows/sparkle/blob/main/media/images/coming%20soon.png?raw=true"}
-                                                //     alt=""
-                                                //     width={2245}
-                                                //     height={1636}
-                                                //     priority
-                                                //     sizes="(min-width: 1024px) 67.8125rem, (min-width: 640px) 100vw, 45rem"
-                                                // />
-                                            }
-                                        </div>
-                                    </Tab.Panel>
-                                ))}
-                            </Tab.Panels>
-                        </>
-                    )}
-                </Tab.Group>
+                    <div className="pr-20 mt-16 "
+                         style={{
+                             flexGrow: 0.6,
+                             flexBasis: 0,
+                             flexShrink: 1,
+                         }}
+                    >
+
+                        <div
+                            className="bg-gradient-to-tr from-red-500  to-blue-900  w-14 h-14 flex items-center justify-center rounded-xl ">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M8.25 3v1.5M4.5 8.25H3m18 0h-1.5M4.5 12H3m18 0h-1.5m-15 3.75H3m18 0h-1.5M8.25 19.5V21M12 3v1.5m0 15V21m3.75-18v1.5m0 15V21m-9-1.5h10.5a2.25 2.25 0 002.25-2.25V6.75a2.25 2.25 0 00-2.25-2.25H6.75A2.25 2.25 0 004.5 6.75v10.5a2.25 2.25 0 002.25 2.25zm.75-12h9v9h-9v-9z"/>
+                            </svg>
+                        </div>
+
+                        <p className="font-bold text-4xl mt-6">
+                            <span
+                                className="bg-gradient-to-tr from-red-300  to-blue-800 text-transparent bg-clip-text">Seamless</span> Access
+                        </p>
+                        <p className="mt-6">
+                            The only entrance point that provides access to
+                            your prompts and tools anywhere and anytime , ensuring convenience and
+                            efficiency in managing your tasks and
+                            resources.
+                        </p>
+                    </div>
+
+                    <div className="flex-1">
+                        <Image
+                            className="basis-0 rounded-xl "
+                            src={writing}
+                            alt={""}/>
+                    </div>
+
+                </div>
+
+                <div
+                    className="flex text-white font-mono mt-60 ">
+
+                    <div className="pr-20 mt-16 flex flex-col  "
+                         style={{
+                             flexGrow: 0.6,
+                             flexBasis: 0,
+                             flexShrink: 1,
+                         }}
+                    >
+
+                        <div
+                            className="bg-gradient-to-bl from-cyan-950 to-emerald-200  w-14 h-14 flex items-center justify-center rounded-xl ">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z"/>
+                            </svg>
+                        </div>
+
+                        <p className="font-bold text-4xl mt-6">
+                            <span
+                                className="bg-gradient-to-tr from-cyan-950 to-emerald-200 text-transparent bg-clip-text">Plugin </span> System
+                        </p>
+                        <p className="mt-6">
+                            With the plugin system, you can easily add new features to your system, and If you are a developer, you can easily create your own plugin and share it with others.
+                        </p>
+                    </div>
+                    <div className="flex-1">
+                        <Image
+                            className="basis-0 rounded-xl "
+                            src={plugin}
+                            alt={""}/>
+                    </div>
+
+
+                </div>
+                <div
+                    className="flex text-white font-mono mt-60 ">
+
+                    <div className="pr-20 mt-16 flex flex-col  "
+                         style={{
+                             flexGrow: 0.6,
+                             flexBasis: 0,
+                             flexShrink: 1,
+                         }}
+                    >
+                        <div
+                            className="bg-gradient-to-l from-purple-800 to-cyan-300  w-14 h-14 flex items-center justify-center rounded-xl ">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605"/>
+                            </svg>
+
+                        </div>
+
+                        <p className="font-bold text-4xl mt-6">
+                            <span
+                                className="bg-gradient-to-tr from-purple-800 to-cyan-300 text-transparent bg-clip-text">Chain </span> Flow
+                        </p>
+                        <p className="mt-6">
+                            Combine the power of plugin with other plugins again and again to
+                            customize
+                            your own powerful workflow </p>
+                    </div>
+                    <div className="flex-1">
+                        <Image
+                            className="basis-0 rounded-xl "
+                            src={chain}
+                            alt={""}/>
+                    </div>
+
+
+                </div>
             </Container>
         </section>
     )
