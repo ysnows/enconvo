4486383b4ccaca413c65aab6cac3428c1b421d3a
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2023-08-06 14:38:26 +0800

    feat: 1. optimize

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -83,14 +83,14 @@ export function Testimonials() {
     <section
       id="testimonials"
       aria-label="What our customers are saying"
-      className="bg-slate-50 py-20 sm:py-32"
+      className="bg-gray-900 py-20 sm:py-32"
     >
       <Container>
         <div className="mx-auto max-w-2xl md:text-center">
-          <h2 className="font-display text-3xl tracking-tight text-slate-900 sm:text-4xl">
+          <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
             Loved by businesses worldwide.
           </h2>
-          <p className="mt-4 text-lg tracking-tight text-slate-700">
+          <p className="mt-4 text-lg tracking-tight text-gray-700">
             Our software is so simple that people can’t help but fall in love
             with it. Simplicity is easy when you just skip tons of
             mission-critical features.
