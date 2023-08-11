b84b3f2d50036bb860d43590f7f937a47860df97
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2023-08-11 10:00:06 +0800

    feat: chang twitter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,7 +21,7 @@ export function Footer() {
         <div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
           <div className="flex gap-x-6">
             <Link
-              href="https://twitter.com/yesterday_snows"
+              href="https://twitter.com/FrostyEveing"
               className="group"
               aria-label="EnConvo on Twitter"
             >
