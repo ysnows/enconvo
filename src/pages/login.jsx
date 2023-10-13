f9a76df2a514a9b9252dd4e733036d285c7e261e
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2023-10-13 21:10:00 +0800

    feat: login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -5,25 +5,66 @@ import {AuthLayout} from '@/components/AuthLayout'
 import {Button} from '@/components/Button'
 import {TextField} from '@/components/Fields'
 import {Logo} from '@/components/Logo'
+import {SUPABASE_ANON_KEY, SUPABASE_URL} from "@/utils/app/const";
+import {useState} from "react";
+import {createClient} from "@supabase/supabase-js";
+import {useRouter} from "next/navigation";
+import {ThemeSupa} from "@supabase/auth-ui-shared";
+import {Auth} from "@supabase/auth-ui-react";
 
 export default function Login() {
+    const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const router = useRouter()
+
+    async function signInWithEmail() {
+
+        const {data, error} = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        })
+        if (data) {
+            router.push('/')
+        }
+    }
+
     return (
         <>
             <Head>
                 <title>Sign In - EnConvo</title>
             </Head>
+
+
             <AuthLayout>
                 <div className="flex flex-col items-center">
                     <Link href="/" aria-label="Home">
                         <Logo className="h-20 w-auto"/>
                     </Link>
-                    <div className="mt-10 flex flex-col items-center">
+                    <div className="mt-10 mb-10 flex flex-col items-center">
                         <h2 className="text-2xl font-semibold text-gray-900">
                             Log in to Enconvo
                         </h2>
                     </div>
                 </div>
-                <form action="#" className="mt-20 grid grid-cols-1 gap-y-8">
+
+                {/*<Auth*/}
+                {/*    supabaseClient={supabase}*/}
+                {/*    providers={[]}*/}
+                {/*    socialLayout={'horizontal'}*/}
+                {/*    // view={'magic_link'}*/}
+                {/*    redirectTo={'/login'}*/}
+                {/*    appearance={{*/}
+                {/*        theme: ThemeSupa, variables: {*/}
+                {/*            default: {},*/}
+                {/*        },*/}
+                {/*    }}*/}
+                {/*/>*/}
+
+
+                <div className="mt-20 grid grid-cols-1 gap-y-8">
                     <TextField
                         label="Email address"
                         id="email"
@@ -31,30 +72,37 @@ export default function Login() {
                         type="email"
                         autoComplete="email"
                         required
+                        onChange={(e) => setEmail(e.target.value)} value={email}
                     />
+
                     <TextField
                         label="Password"
                         id="password"
                         name="password"
                         type="password"
                         autoComplete="current-password"
+                        onChange={(e) => setPassword(e.target.value)} value={password}
                         required
                     />
+
                     <div>
                         <Button
                             type="submit"
                             variant="solid"
                             color="blue"
                             className="w-full"
+                            onClick={signInWithEmail}
                         >
-              <span>
-                Sign in <span aria-hidden="true">&rarr;</span>
-              </span>
+                        <span>
+                          Sign in <span aria-hidden="true">&rarr;</span>
+                        </span>
                         </Button>
-                    </div>
-                </form>
 
-                <p  className="mt-2 ml-1 text-sm text-gray-700">
+
+                    </div>
+                </div>
+
+                <p className="mt-2 ml-1 text-sm text-gray-700">
                     Don’t have an account?{' '}
                     <Link
                         href="/register"
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,101 +1,113 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
-import { AuthLayout } from '@/components/AuthLayout'
-import { Button } from '@/components/Button'
-import { SelectField, TextField } from '@/components/Fields'
-import { Logo } from '@/components/Logo'
+import {AuthLayout} from '@/components/AuthLayout'
+import {Button} from '@/components/Button'
+import {SelectField, TextField} from '@/components/Fields'
+import {Logo} from '@/components/Logo'
+import {createClient} from "@supabase/supabase-js";
+import {SUPABASE_ANON_KEY, SUPABASE_URL} from "@/utils/app/const";
+import {useState} from "react";
+import {useRouter} from "next/navigation";
 
 export default function Register() {
-  return (
-    <>
-      <Head>
-        <title>Sign Up - EnConvo</title>
-      </Head>
-      <AuthLayout>
-        <div className="flex flex-col">
-          <Link href="/" aria-label="Home">
-            <Logo className="h-10 w-auto" />
-          </Link>
-          <div className="mt-20">
-            <h2 className="text-lg font-semibold text-gray-900">
-              Get started for free
-            </h2>
-            <p className="mt-2 text-sm text-gray-700">
-              Already registered?{' '}
-              <Link
-                href="/login"
-                className="font-medium text-blue-600 hover:underline"
-              >
-                Sign in
-              </Link>{' '}
-              to your account.
-            </p>
-          </div>
-        </div>
-        <form
-          action="#"
-          className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2"
-        >
-          <TextField
-            label="First name"
-            id="first_name"
-            name="first_name"
-            type="text"
-            autoComplete="given-name"
-            required
-          />
-          <TextField
-            label="Last name"
-            id="last_name"
-            name="last_name"
-            type="text"
-            autoComplete="family-name"
-            required
-          />
-          <TextField
-            className="col-span-full"
-            label="Email address"
-            id="email"
-            name="email"
-            type="email"
-            autoComplete="email"
-            required
-          />
-          <TextField
-            className="col-span-full"
-            label="Password"
-            id="password"
-            name="password"
-            type="password"
-            autoComplete="new-password"
-            required
-          />
-          <SelectField
-            className="col-span-full"
-            label="How did you hear about us?"
-            id="referral_source"
-            name="referral_source"
-          >
-            <option>AltaVista search</option>
-            <option>Super Bowl commercial</option>
-            <option>Our route 34 city bus ad</option>
-            <option>The “Never Use This” podcast</option>
-          </SelectField>
-          <div className="col-span-full">
-            <Button
-              type="submit"
-              variant="solid"
-              color="blue"
-              className="w-full"
-            >
+
+    const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const router = useRouter()
+
+    const handleSignUp = async () => {
+        const user = await supabase.auth.signUp({
+            email,
+            password,
+            options: {
+                emailRedirectTo: `${location.origin}/login`,
+            },
+        })
+        console.log(user)
+    }
+
+
+    return (
+        <>
+            <Head>
+                <title>Sign Up - EnConvo</title>
+            </Head>
+            <AuthLayout>
+                <div className="flex flex-col items-center">
+                    <Link href="/" aria-label="Home">
+                        <Logo className="h-20 w-auto"/>
+                    </Link>
+
+                    <h2 className="mt-10 text-2xl font-semibold text-gray-900">
+                        Get started for free
+                    </h2>
+                </div>
+                <form
+                    action="#"
+                    className="mt-20 grid grid-cols-1 gap-x-6 gap-y-8 "
+                >
+                    <TextField
+                        label="Name"
+                        id="name"
+                        name="name"
+                        type="text"
+                        autoComplete="name"
+                        required
+                        onChange={(e) => setName(e.target.value)} value={name}
+
+                    />
+
+                    <TextField
+                        className="col-span-full"
+                        label="Email address"
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        onChange={(e) => setEmail(e.target.value)} value={email}
+                        required
+                    />
+                    <TextField
+                        className="col-span-full"
+                        label="Password"
+                        id="password"
+                        name="password"
+                        type="password"
+                        autoComplete="new-password"
+                        onChange={(e) => setPassword(e.target.value)} value={password}
+                        required
+                    />
+                    <div className="col-span-full">
+                        <Button
+                            onClick={handleSignUp}
+                            type="submit"
+                            variant="solid"
+                            color="blue"
+                            className="w-full"
+                        >
               <span>
                 Sign up <span aria-hidden="true">&rarr;</span>
               </span>
-            </Button>
-          </div>
-        </form>
-      </AuthLayout>
-    </>
-  )
+                        </Button>
+                    </div>
+                </form>
+
+                <p className="mt-2 text-sm text-gray-700">
+                    Already registered?{' '}
+                    <Link
+                        href="/login"
+                        className="font-medium text-blue-600 hover:underline"
+                    >
+                        Sign in
+                    </Link>{' '}
+                    to your account.
+                </p>
+
+            </AuthLayout>
+        </>
+    )
 }
