0fd3db70a98bfd3130395022ecde8d398663227f
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2023-10-29 01:40:15 +0800

    feat: 133

--- src/utils/app/native_router.ts
+++ src/utils/app/native_router.ts
@@ -3,5 +3,9 @@ export const NativeRouter = {
     login: (accessToken: string, refreshToken: string) => {
         window.location.href = `enconvo://login?access_token=${accessToken}&refresh_token=${refreshToken}`;
 
+    },
+
+    install: (name: string, title: string, downloadUrl: string) => {
+        window.location.href = `enconvo://install?name=${name}&title=${title}&downloadUrl=${downloadUrl}`;
     }
 };
