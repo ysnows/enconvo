5461125f1492f2fd754fa2436449bc3cd32f9ecf
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2023-11-07 06:34:27 +0800

    1.app update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -229,7 +229,7 @@ export function Hero() {
 
                             <div className="mt-4 text-sm font-normal text-gray-500 space-x-4">
                                 <span
-                                    className=" pb-3  pl-1">v1.3.9</span>
+                                    className=" pb-3  pl-1">v1.4.0</span>
                                 <span className="pb-3  pl-1 text-xs">|</span>
                                 <span
                                     className=" pb-3  pl-1">macOS 13+</span>
